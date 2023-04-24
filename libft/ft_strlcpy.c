c57e55acf1b489d72f82cbe86dad6468dce6489b
Alba Arjona Obrero <121249204+AlbaArjonaObrero@users.noreply.github.com>
2023-04-24 15:30:18 +0200

    Update ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alarjona <alarjona@student.42malaga.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/21 10:22:39 by alarjona          #+#    #+#             */
-/*   Updated: 2023/03/22 13:09:12 by alarjona         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <string.h>
 #include <stdio.h>
 
@@ -26,9 +14,3 @@ char	*ft_strcpy(char *dest, char *src)
 	dest[a] = '\0';
 	return (dest);
 }
-/*int main ()
-{
-	char	dest[] = "Holmundo";
-	char 	src[]= "malaga";
-	printf("%s", ft_strcpy(dest, src));
-}*/
