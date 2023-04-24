143468f51c97467afcebff951ac76bd4cccb1f82
Alba Arjona Obrero <121249204+AlbaArjonaObrero@users.noreply.github.com>
2023-04-24 15:30:48 +0200

    Update ft_strchr.c

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,27 +1,7 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alarjona <alarjona@student.42malaga.com>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/19 17:01:14 by alarjona          #+#    #+#             */
-/*   Updated: 2023/04/19 17:01:16 by alarjona         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
-/*
-** La función ft_strrchr busca la última aparición de un carácter en una cadena.
-** Si encuentra el carácter, devuelve un puntero al carácter encontrado.
-** Si no encuentra el carácter, devuelve un puntero nulo (NULL).
-**
-** str: cadena de caracteres en la que buscar el carácter.
-** c: carácter a buscar.
-*/
 
 int	ft_strlen(const char *str)
 {
@@ -53,12 +33,3 @@ char *ft_strrchr(const char *str, int c)
 	}
 	return (NULL);
 }
-
-/*int main (void)
-{
-	char str[] = "Hola Mundo";
-	char d = 'o';
-	char *ptr = ft_strrchr(str, d);
-	printf ("%ld\n", ptr - str);
-	return (0);
-}*/
