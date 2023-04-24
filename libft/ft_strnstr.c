cd1918cbe46db158b45f6a5f619849cf4a05faf5
Alba Arjona Obrero <121249204+AlbaArjonaObrero@users.noreply.github.com>
2023-04-24 15:29:12 +0200

    Update ft_strnstr.c

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,16 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alarjona <alarjona@student.42malaga.com>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/24 10:55:27 by alarjona          #+#    #+#             */
-/*   Updated: 2023/04/24 10:55:29 by alarjona         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-
 #include <stdio.h>
 
 char	*ft_strnstr(const char *large, const char *small, size_t len)
@@ -37,22 +24,4 @@ char	*ft_strnstr(const char *large, const char *small, size_t len)
 	return (0);
 }
 
-/*int main(void)
-{
 
-    char str1[] = "Hola mundo";
-    char str2[] = "m";
-    char *result;
-
-    result = ft_strnstr(str1, str2, 8);
-	printf("%s", result);
-	return(0);
-
-    // Comprobación del resultado devuelto por la función ft_strnstr
-    if (result == str1) // Si el resultado es igual al puntero al inicio de la cadena
-        printf("La función ha devuelto el puntero al inicio de la cadena\n");
-    else // Si el resultado no es igual al puntero al inicio de la cadena
-        printf("La función NO ha devuelto el puntero al inicio de la cadena\n");
-    // Salida del programa
-    return (0);
-}*/
